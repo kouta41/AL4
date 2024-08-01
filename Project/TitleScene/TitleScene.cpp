e2dcede73377580fc6cfb076879abe3f923f848e
kt610 <kt610@MSI>
2024-08-01 13:01:31 +0900

    save

--- Project/TitleScene/TitleScene.cpp
+++ Project/TitleScene/TitleScene.cpp
@@ -83,18 +83,6 @@ void TitleScene::Draw(){
 	//プレイヤーの描画
 //	player_->Draw(camera);
 
-	ImGui::Begin("Player");
-	if (ImGui::TreeNode("worldTransform")) {
-		ImGui::DragFloat3("translate", &worldTransform.translate.x, 0.1f, 100, 100);
-		ImGui::DragFloat3("rotate", &worldTransform.rotate.x, 0.01f, -6.28f, 6.28f);
-		ImGui::DragFloat3("scale", &worldTransform.scale.x, 0.01f, 0, 10);
-		ImGui::TreePop();
-	}
-	
-	ImGui::DragFloat("animationTime", &animationTime,0.01f);
-
-	ImGui::End();
-
 	ImGui::Begin("Camera");
 	if (ImGui::TreeNode("transform")) {
 		ImGui::DragFloat3("translate", &camera.translate.x, 0.1f, 100, 100);
@@ -104,6 +92,16 @@ void TitleScene::Draw(){
 	ImGui::End();
 	
 
+	ImGui::Begin("player");
+	if (ImGui::TreeNode("worldTransform")) {
+		ImGui::DragFloat3("translate", &worldTransform.translate.x, 0.1f, 100, 100);
+		ImGui::DragFloat3("rotate", &worldTransform.rotate.x, 0.01f, -6.28f, 6.28f);
+		ImGui::DragFloat3("scale", &worldTransform.scale.x, 0.01f, 0, 10);
+		ImGui::TreePop();
+	}
+
+	ImGui::DragFloat("animationTime", &animationTime, 0.01f);
+	ImGui::End();
 
 //	matio_1->Draw(worldTransform1, camera);
 	motion_->Draw(worldTransform,camera);
